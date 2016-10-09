65ffd36220ab23d7410a7cf528436d4a6dbd42ab
Jeremy Odell <jeremyodell@Jeremys-MacBook-Pro.local>
2016-10-09 13:12:16 -0500

    1dell branch change

--- quotes/ViewController.swift
+++ quotes/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // this is Jeremy1dell branch
     }
 
     override func didReceiveMemoryWarning() {
