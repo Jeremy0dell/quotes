256bfed231c9531402297f7d2a9e2b4e0c08264d
Jeremy Odell <jeremyodell@Jeremys-MacBook-Pro.local>
2016-10-09 13:00:02 -0500

    1dell - Add new View Controller

---
+++ quotes/Jeremy1dellViewController.swift
@@ -0,0 +1,11 @@
+//
+//  Jeremy1dellViewController.swift
+//  quotes
+//
+//  Created by Jeremy Odell on 10/9/16.
+//  Copyright © 2016 Jeremy Odell. All rights reserved.
+//
+
+import Foundation
+
+// I am Jeremy 1dell and I create a lot of code on the Jeremy1dellViewController
--- quotes/ViewController.swift
+++ quotes/ViewController.swift
@@ -19,7 +19,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // I am Jeremy 1dell and I create a lot of code
 
 }
 
